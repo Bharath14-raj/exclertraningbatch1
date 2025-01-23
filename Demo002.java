511114b5dfde5b97f5e0245b8865891b6b9aa139
Bharath14-raj <nampallybharath65@gmail.com>
2025-01-23 19:12:27 +0530

    greatest of 3 numbers

---
+++ Demo002.java
@@ -0,0 +1,14 @@
+public class Demo002 {
+    
+
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 6;
+        if (x > y) {
+            System.out.println(x);
+        } else {
+            System.out.println(y);
+        }
+
+    }
+}

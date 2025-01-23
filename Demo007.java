c26b9bab4fdf3b75a2f2821fa564b19410258019
Bharath14-raj <nampallybharath65@gmail.com>
2025-01-23 19:18:31 +0530

    Printing Name, Age and Phone number

---
+++ Demo007.java
@@ -0,0 +1,13 @@
+
+
+public class Demo007 {
+    
+    public static void main(String[] args) {
+        String Name = "Bharath";
+        int Age = 20;
+        long PhNO = 1234567890;
+        while ((Name == "Bharath") && (Age == 20) && (PhNO == 1234567890)) {
+            break;
+        }
+    }
+}

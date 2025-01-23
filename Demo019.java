7782c493e032f75c772c9e55ab0b4a32d76d1a43
Bharath14-raj <nampallybharath65@gmail.com>
2025-01-23 19:25:53 +0530

    fizzbuzz

---
+++ Demo019.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class Demo19 {
+   
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.err.println("Enter a number: ");
+            int n = sc.nextInt();
+             
+            int first = 0, second = 1;
+    
+            System.out.println("Fibonacci Series up to " + n + " terms:");
+    
+            
+            for (int i = 1; i <= n; ++i) {
+                System.out.print(first + " ");
+    
+                
+                int nextTerm = first + second;
+                first = second;
+                second = nextTerm;
+            }
+        }
+    }
+

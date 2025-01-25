1f05b6abe42417f05cca475db908152fe66a614c
Bharath14-raj <nampallybharath65@gmail.com>
2025-01-25 11:19:33 +0530

    big number between 3 numbers

---
+++ Demo036.java
@@ -0,0 +1,22 @@
+public class Demo036 {
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        long num1 = scanner.nextLong();
+
+        System.out.print("Enter the second number: ");
+        long num2 = scanner.nextLong();
+        System.out.print("Enter the third number: ");
+        long num3 = scanner.nextLong();
+        if (num1 > num2 && num1 > num3) { 
+            System.out.println(num1 + " is larger.");
+        } else if (num2 > num1 && num2 > num3) {
+            System.out.println(num2 + " is larger.");
+        } else {
+            System.out.println(num3 + " is larger.");
+        }
+
+        scanner.close();
+    }
+}
